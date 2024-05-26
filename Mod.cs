5d7223396406dccd71930bbc4321f5bc89b161be
Shinter <shinter@noreply.com>
2024-05-26 06:17:00 +0400

    MPUIWhitelist Mod Init

---
+++ Mod.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using ABI_RC.Core.Util.AssetFiltering;
+
+using HarmonyLib;
+
+using MelonLoader;
+
+using MPUIKIT;
+namespace MPUIWhitelist
+{
+
+    public static class ModBuildInfo
+    {
+        public const string Name = "MPUIWhitelist";
+        public const string Author = "Shin";
+        public const string Version = "1.0.0";
+        public const string DownloadLink = "none";
+        public const string Description = "This mod allows you to use MPUIKit on props!";
+
+    }
+    
+    public class MPUIWhitelist : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg(ModBuildInfo.Description);
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (buildIndex == -1)
+            {
+                var whitelist = Traverse.Create(typeof(SharedFilter)).Field<HashSet<Type>>("_spawnableWhitelist").Value;
+                try
+                {
+
+                    whitelist.Add(typeof(MPImage));
+                }
+                catch (Exception ex)
+                {
+                    // SILENCE I DON'T WANT TO HEAR ABOUT YOUR NULL REFERENCE ISSUE!!!
+                }
+                
+            }
+        }
+    }
+}
+
+   
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using MPUIWhitelist;
+
+
+
+
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: MelonInfo(typeof(MPUIWhitelist.MPUIWhitelist), ModBuildInfo.Name, ModBuildInfo.Version, ModBuildInfo.Author, ModBuildInfo.DownloadLink)]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: MelonColor(255, 0, 255, 0)]
+#pragma warning restore CS0618 // Type or member is obsolete
+[assembly: MelonPriority(int.MaxValue)]
